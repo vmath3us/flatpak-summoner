640cdd6445e2281f85491118d03f24a0875f2445
vmath3us <vmatheusborges@gmail.com>
2023-03-22 10:08:32 -0400

    Escape special characters, close stder

--- flatpak-summoner.sh
+++ flatpak-summoner.sh
@@ -4,41 +4,45 @@ command_not_found_handle() {
     ###### if command not exists on path, run
     #
 flatpak_and_distrobox_aux_bin_handler "${@}"
-# don't run if not in a container
 }
 flatpak_and_distrobox_aux_bin_handler(){ #############direct zsh version
     if [ $FLT_DTB_HANDLER_DEBUG -eq "0" ] ; then
-        end_command='>/dev/null 2>/dev/null &'   #### detached process, not output, close terminal not kill program.
-        #####close std(out/err) using 1>&- 2>&- crash vscode and others, redirect to dev/null by default
+        end_command='>/dev/null 2>/dev/null &'   #### detached process, not output, close terminal not kill program. close std(out/err) using 1>&- 2>&- crash vscode and others, redirect to dev/null by default
+        end_find_command='2>&-' #### close find stderror by default
     fi
+flatpak_command="flatpak run"
 #################################################################################################
 #################################################################################################
 #################################################################################################
 #################################################################################################
-######## if your not using distrobox (github.com/89luca89/distrobox) , preserve line 2
-  if [ ! -e /run/.containerenv ] && [ ! -e /.dockerenv ]; then # 1
-      flatpak_command="flatpak run"     ### on host using flatpak # 2
+######## if your not using distrobox (github.com/89luca89/distrobox) , remove this session
+  if [ ! -e /run/.containerenv ] && [ ! -e /.dockerenv ]; then
+      flatpak_command="flatpak run"     ### on host using flatpak
   else # 3
-      dhe="$(which distrobox-host-exec)" ##### is possible install distrobox on /usr/local,/usr, or ~/.local/bin # 4
-      $dhe "${@}" 2>/dev/null || flatpak_command="$dhe flatpak run" #if on container, try on host, or on host using flatpak # 5
-  fi # 6
+      dhe="$(which distrobox-host-exec)" ##### is possible install distrobox on /usr/local,/usr, or ~/.local/bin
+      $dhe "${@}" 2>/dev/null || flatpak_command="$dhe flatpak run" #if on container, try on host, or on host using flatpak
+  fi
 #################################################################################################
 #################################################################################################
 #################################################################################################
 #################################################################################################
     cmd=$1 ###################-preserve name command to search
     shift ####################-remaining entry (path or parameter) on $@
+    args=(${@})     ###### needed to zsh. https://stackoverflow.com/a/72144680
+    for i in ${args[@]}; do
+        entry="${entry} \"${i}\""
+    done   ####### escape special characters, on links or files. eval try expand = , ?, etc
 ########## flatpak list --app is slow, find directly on bin path
 flatpak_bin_dir[1]="${HOME}/.local/share/flatpak/exports/bin"
-flatpak_bin_dir[2]="/var/lib/flatpak/exports/bin" ##### -- user as precedence . Edit to using only user, or only system . if there are the same names for user and system, the user's appear first
-    path_bin=($(find $flatpak_bin_dir -maxdepth 1 -iname "*"${cmd}"*" -printf '%P\n')) ##### search command
+flatpak_bin_dir[2]="/var/lib/flatpak/exports/bin" ##### -- user as precedence. Comment to disable. if there are the same names for user and system, the user's appear first
+path_bin=($(find ${flatpak_bin_dir[@]} -maxdepth 1 -iname "*"${cmd}"*" -printf '%P\n' $end_find_command)) ##### search command ,stderror closed by default
     if [ "${#path_bin[@]}" -eq "1" ]; then   ##### if there is only one result ,run
-        final_command="$flatpak_command ${path_bin} "${@}" $end_command" #### create command
+      final_command="$flatpak_command ${path_bin} ${entry} $end_command" #### create command
        printf "$final_command%s\n" ###### show command
-       eval ${final_command} ####### finally run
+      eval ${final_command} ####### finally run
     elif [ "${#path_bin[@]}" -gt "1" ] ; then ########### else, ask why
         select choice in ${path_bin[@]}; do
-            final_command="$flatpak_command $choice "${@}" $end_command"  ### create command
+        final_command="$flatpak_command ${path_bin} "${entry}" $end_command" #### create command
             printf "$final_command%s\n" ###### show command
             eval ${final_command} ############ finally run
             break       #### escape to select loop
